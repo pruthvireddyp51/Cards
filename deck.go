0f7b0081674777ebfa775a1de2cc8ebb57f48bfc
pruthvi <pruthvireddyp51@gmail.com>
2021-11-20 17:01:39 +0530

    Intial commit changes

---
+++ deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string {"h1", "h2", "h3"}
+	cardValues := []string {"v1", "v2", "v3"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// Reciver on a function
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+func main() {
+	cards := deck{"Hello", cardName()}
+	cards = newDeck()
+	// range is a default keyword 
+	// for i, card := range cards{
+	// 	fmt.Println(i, card)
+	// }
+	// fmt.Println(cards)
+	cards.print() 
+}
+
+func cardName() string {
+	return "Name of the card"
+}
