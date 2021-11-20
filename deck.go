c1661ad0c70afbfe0bcd0b2322b00fac8823dd13
Pruthvi <pruthvireddyp51@gmail.com>
2021-11-20 19:42:35 +0530

    align ment changes

--- deck.go
+++ deck.go
@@ -4,12 +4,11 @@ import "fmt"
 
 type deck []string
 
-
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string {"h1", "h2", "h3"}
-	cardValues := []string {"v1", "v2", "v3"}
+	cardSuits := []string{"h1", "h2", "h3"}
+	cardValues := []string{"v1", "v2", "v3"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
--- main.go
+++ main.go
@@ -5,12 +5,12 @@ package main
 func main() {
 	cards := deck{"Hello", cardName()}
 	cards = newDeck()
-	// range is a default keyword 
+	// range is a default keyword
 	// for i, card := range cards{
 	// 	fmt.Println(i, card)
 	// }
 	// fmt.Println(cards)
-	cards.print() 
+	cards.print()
 }
 
 func cardName() string {
